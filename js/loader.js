cfd367506e9fcf377e0c433d8ede5c8ea86feb23
Alan Leeson <aml9227@rit.edu>
2015-03-30 15:32:11 -0400

    initial commit with resizable canvas. Bare bones to make a mobile game. Much like the start of 'bubbles'

---
+++ js/loader.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var app = app || {};
+
+window.onload = function() {
+	app.shapeShatter.init();
+	
+	window.onresize = function(){
+		app.shapeShatter.resize();
+	};
+	
+	window.addEventListener('mousedown',function(e){
+		e.preventDefault();
+		app.shapeShatter.setInput(e);
+	}, false);
+	
+	window.addEventListener('touchstart',function(e){
+		e.preventDefault();
+		app.shapeShatter.setInput(e.touches[0]);
+	},false);
+	
+	window.addEventListener('touchmove',function(e){
+		e.preventDefault();
+	},false);
+	
+	window.addEventListener('touchend',function(e){
+		e.preventDefault();
+	},false);
+};
---
+++ js/shapeShatter.js
@@ -0,0 +1,83 @@
+"use strict"
+
+var app = app || {};
+
+app.shapeShatter = {
+
+	WIDTH : 320, 
+    HEIGHT: 480,
+
+	RATIO : undefined,
+	currentWidth : undefined,
+	currentHeight : undefined,
+	canvas : undefined,
+	ctx :  undefined,
+	
+	init : function(){
+	
+		this.RATIO = this.WIDTH / this.HEIGHT;
+		
+		this.currentWidth = this.WIDTH;
+		this.currentHeight = this.HEIGHT;
+		
+		// this is our canvas element
+		this.canvas = document.querySelector('canvas');
+		this.canvas.width = this.WIDTH;
+		this.canvas.height = this.HEIGHT;
+		
+		this.ctx = this.canvas.getContext('2d');
+		
+		this.ua = navigator.userAgent.toLowerCase();
+		this.android = this.ua.indexOf('android') > -1 ? true : false;
+		this.ios = ( this.ua.indexOf('iphone') > -1 || this.ua.indexOf('ipad') > -1 || this.ua.indexOf('ipod') > -1 ) ? true : false;
+		
+		this.resize();
+		this.gameLoop();
+	},
+	
+	gameLoop : function(){
+		requestAnimationFrame(this.gameLoop.bind(this));
+    	this.render();
+	},
+	
+	render : function(){
+		
+    },
+    
+    setInput : function(data){
+    	//this.xTap = (data.pageX - this.offset.left)/this.scale;
+    	//this.yTap = (data.pageY - this.offset.top)/this.scale;
+    	//this.tapped = true;
+    },
+
+	resize : function() {
+     	console.log("resize");
+        this.currentHeight = window.innerHeight;
+        // resize the width in proportion
+        // to the new height
+        this.currentWidth = this.currentHeight * this.RATIO;
+
+    
+        // set the new canvas style width and height
+        // note: our canvas is still 320 x 480, but
+        // we're essentially scaling it with CSS
+        this.canvas.style.width = this.currentWidth + 'px';
+        this.canvas.style.height = this.currentHeight + 'px';
+        
+         // this will create some extra space on the
+        // page, allowing us to scroll past
+        // the address bar, thus hiding it.
+        if (this.android || this.ios) {
+            document.body.style.height = (window.innerHeight + 50) + 'px';
+        }
+
+        // we use a timeout here because some mobile
+        // browsers don't fire if there is not
+        // a short delay
+        window.setTimeout(function() {
+                window.scrollTo(0,1);
+        }, 10);
+        
+    }
+
+};
