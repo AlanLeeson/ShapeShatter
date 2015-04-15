eb44bb3aa5b09bdfa13b5246aea8bbdd1ef70ac4
Alan Leeson <aml9227@rit.edu>
2015-04-14 23:46:13 -0400

    Forgot to switch back to starting with tutorial

--- js/shapeShatter.js
+++ js/shapeShatter.js
@@ -72,7 +72,7 @@ app.shapeShatter = {
 		this.android = this.ua.indexOf('android') > -1 ? true : false;
 		this.ios = ( this.ua.indexOf('iphone') > -1 || this.ua.indexOf('ipad') > -1 || this.ua.indexOf('ipod') > -1 ) ? true : false;
 		
-		this.gameState = this.GAME_STATE_MENU;
+		this.gameState = this.GAME_STATE_TUTORIAL;
 		
 		//makes the menu button
 		this.menuButton = new app.InputButton("menu",this.WIDTH/2,400,40,"Menu",false,
